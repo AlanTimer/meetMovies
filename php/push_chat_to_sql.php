1ac2a8dc4ba9c0b5669856af84740136ff08a0a1
jkz <2743311904@qq.com>
2019-06-28 10:26:59 +0800

    数据库中写入信息

--- php/push_chat_to_sql.php
+++ php/push_chat_to_sql.php
@@ -1,12 +1,9 @@
 <?php
 //session_start();
 require "config.php";
-//$from=$_SESSION['user']['Id'];      //发送者的Id
-//$to=$_POST['to'];                   //接受者的Id
-//$message=$_POST['message'];         //信息内容
-$from=1;      //发送者的Id
-$to=2;                   //接受者的Id
-$message='d';         //信息内容
+$from=$_SESSION['user']['Id'];      //发送者的Id
+$to=$_POST['to'];                   //接受者的Id
+$message=$_POST['message'];         //信息内容
 $time=time();                        //当前时间
 
 try{
@@ -17,7 +14,7 @@ try{
     echo $e->getMessage();
 }
 //chats表中增加语句
-$sql = "insert into chats (from,to,message,time) values('$from','$to','$message','$time')";
+$sql = "insert into chats(from_id,to_id,message,send_time) values('$from','$to','$message','$time')";
 $res = $pdo->prepare($sql);
 $res->execute();
 
