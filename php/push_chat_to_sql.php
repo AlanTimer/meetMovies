3baa6bf38f25e829dde7358798a10904adf6171f
jkz <2743311904@qq.com>
2019-06-28 10:06:23 +0800

    好友完善

--- php/push_chat_to_sql.php
+++ php/push_chat_to_sql.php
@@ -1,26 +1,25 @@
 <?php
-session_start();
+//session_start();
 require "config.php";
 //$from=$_SESSION['user']['Id'];      //发送者的Id
 //$to=$_POST['to'];                   //接受者的Id
 //$message=$_POST['message'];         //信息内容
-$time=time();                       //当前时间
-print_r($time);
+$from=1;      //发送者的Id
+$to=2;                   //接受者的Id
+$message='d';         //信息内容
+$time=time();                        //当前时间
 
-//try{
-//    //连接数据库、选择数据库
-//    $pdo = new PDO(DB_DSN,DB_USER,DB_PWD);
-//}catch(PDOException $e){
-//    //输出异常信息
-//    echo $e->getMessage();
-//}
-////user表中查找id
-//$sql = 'select friend from friends where user = :user && friend = :friend';
-//$res = $pdo->prepare($sql);
-//$res->bindParam(':user',$user);//绑定参数
-//$res->bindParam(':friend',$user);//绑定参数
-//$res->execute();
-//$friends_id = $res->fetch(PDO::FETCH_NUM);  //返回一个索引为结果集列名的数组
-//
-//
-//
+try{
+    //连接数据库、选择数据库
+    $pdo = new PDO(DB_DSN,DB_USER,DB_PWD);
+}catch(PDOException $e){
+    //输出异常信息
+    echo $e->getMessage();
+}
+//chats表中增加语句
+$sql = "insert into chats (from,to,message,time) values('$from','$to','$message','$time')";
+$res = $pdo->prepare($sql);
+$res->execute();
+
+
+
