b26fd2e3d0cfa2c218ee50c3b70802d1d82b25cd
jkz <2743311904@qq.com>
2019-07-01 21:16:21 +0800

    小星星

--- movie_details.php
+++ movie_details.php
@@ -1,8 +1,8 @@
 <?php
 require "php/config.php";  //导入数据库配置文件
 header("Content-type:text/html;charset=utf-8"); //utf-8
-$movie_name=$_POST['movie_name'];//存在接受
-//$movie_name='霸王别姬';//存在接受
+//$movie_name=$_POST['movie_name'];//存在接受
+$movie_name='霸王别姬';//存在接受
 //建立PDO数据库连接，查询数据
 $pdo=new PDO(DB_DSN,DB_USER,DB_PWD);
 $sql="select  * from movies where name = '$movie_name' ";
